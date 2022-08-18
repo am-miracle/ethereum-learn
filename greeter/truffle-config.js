21f1693cec571229f10013467788b097a4eac488
Miracle Jude <judemiraco0@gmail.com>
2022-08-18 23:17:06 +0100

    final commit

--- greeter/truffle-config.js
+++ greeter/truffle-config.js
@@ -18,7 +18,11 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+ require("dotenv").config({
+  path: `.env`,
+})
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
@@ -33,6 +37,8 @@ module.exports = {
    *
    * $ truffle test --network <network-name>
    */
+   contracts_build_directory: "./client/src/contracts",
+
 
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
@@ -40,12 +46,18 @@ module.exports = {
     // You should run a client (like ganache, geth, or parity) in a separate terminal
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
-    //
     // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    //   host: "127.0.0.1",     // Localhost (default: none)
+    //   port: 7545,            // Standard Ethereum port (default: none)
+    //   network_id: "5777",       // Any network (default: none)
+    //  },
+     goerli: {
+      provider: () => {
+        const mnemonic = process.env.MNEMONIC
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545");
+      },
+      network_id: "*",
+     },
     //
     // An additional network, but with some advanced options…
     // advanced: {
@@ -60,13 +72,24 @@ module.exports = {
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
     // ropsten: {
-    //   provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
+      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
     //   network_id: 3,       // Ropsten's id
     //   gas: 5500000,        // Ropsten has a lower block limit than mainnet
     //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
     //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
     //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
+    rinkeby: {
+      provider: () => {
+        const mnemonic = process.env.MNEMONIC
+        const project_id = process.env.PROJECT_ID
+        return new HDWalletProvider(
+          mnemonic,
+          `https://rinkeby.infura.io/v3/${project_id}`
+        );
+      },
+      network_id: "*"
+     }
     //
     // Useful for private networks
     // private: {
